7521ab99819529ebe6cb35f0376e749dfc5e6b8b
GilbertKabunyu <gkabunyuk15@gmail.com>
2024-06-24 13:33:48 +0200

    chamber index html and styling css

---
+++ chamber/scripts/lastModified.js
@@ -0,0 +1,4 @@
+
+// Get the last modified date and update the footer
+const lastModification = document.getElementById('lastModified');
+lastModification.textContent = `Last Modification: ${document.lastModified}`;
